1cf40819a79827b702c5bad296f46152882851de
r0o0 <rayeoh7@gmail.com>
2019-09-16 17:51:31 +0900

    update header nav component to be responsive

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,25 +1,30 @@
 <template lang="pug">
   header
-    h1.logo Vmarket
+    h1.logo Vshop
     template(v-if="width < 768")
       div.header-right
-        i.fa.fa-bars
-        i.fa.fa-search
+        c-nav(:screenWidth="width")
+        //- span.category 메뉴
+        //-   i.el-icon-menu.i-menu
+        c-search(:screenWidth="width")
         i.el-icon-shopping-cart-2.i-cart
     template(v-else)
-      c-nav.nav-container
+      c-nav(:screenWidth="width")
       div.header-right
-        el-input.input-search(placeholder="검색해보세요." v-model="input")
+        c-search
+
         i.el-icon-shopping-cart-2.i-cart
 </template>
 
 <script>
 import CNav from './Nav'
+import CSearch from './Search'
 
 export default {
   name: 'CHeader',
   components: {
-    CNav
+    CNav,
+    CSearch
   },
   data () {
     return {
@@ -49,6 +54,9 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../sass/partials/fonts';
+@import '../../sass/partials/mixin';
+@import '../../sass/partials/color';
+
   header {
     padding: 20px 0;
     display: flex;
@@ -56,21 +64,56 @@ export default {
     justify-content: flex-start;
     align-items: center;
     width: 100%;
+    min-height: 70px;
     .logo {
       font-size: $font-xl;
       font-weight: 700;
+      color: $color-point;
     }
-    .nav-container {
-      margin-left: 40px;
-    }
+    // .nav {
+    //   margin-left: 40px;
+    //   @include mobile {
+    //     margin-left: 0;
+    //   }
+    // }
     .header-right {
       display: flex;
-      flex-flow: row nowrap;
       align-content: flex-end;
       align-items: center;
       margin-left: auto;
-      .i-cart {
+      & > * {
         margin-left: 16px;
+        color: $color-light;
+      }
+      @include mobile {
+        flex-direction: row-reverse;
+        i.i-search {
+          order: 2;
+        }
+        i.i-cart {
+          order: 1;
+        }
+        .category {
+          display: flex;
+          align-items: center;
+          margin-left: 24px;
+          font-size: 15px;
+          color: $color-lighter;
+          i.i-menu {
+            order: -1;
+            padding-right: 5px;
+            font-size: 21px;
+            color: $color-point;
+          }
+        }
+      }
+      @include desktop {
+        flex-flow: row nowrap;
+      }
+      .i-search {
+        font-size: 18px;
+      }
+      .i-cart {
         font-size: $font-l;
       }
     }
--- src/components/common/nav.vue
+++ src/components/common/nav.vue
@@ -1,23 +1,68 @@
 <template lang="pug">
-  nav
-    ul.nav-container
-      li.nav-item
-        router-link(to="/my") 채소
-      li.nav-item 과일
-      li.nav-item 소스
+  div.nav
+    template(v-if="screenWidth < 768")
+      span.category(@click="trigger=true") 메뉴
+        i.el-icon-menu.i-menu
+      el-drawer(
+        :visible.sync="trigger"
+        :direction="'ttb'"
+        :title="'VShop'"
+      )
+        nav
+          ul.nav-container
+            li.nav-item
+              router-link(to="/my") 채소
+            li.nav-item 과일
+            li.nav-item 소스
+        el-button(plain) 로그아웃
+    nav(v-else)
+      ul.nav-container
+        li.nav-item
+          router-link(to="/my") 채소
+        li.nav-item 과일
+        li.nav-item 소스
 </template>
 
 <script>
 export default {
-  name: 'CNav'
+  name: 'CNav',
+  props: ['screenWidth'],
+  data () {
+    return {
+      trigger: false
+    }
+  }
 }
 </script>
 
-<style lang="sass">
-  .nav-container
-    display: flex
-    flex-flow: row wrap
-    & .nav-item
-      margin-right: 24px
-      list-style: none
+<style lang="scss" scoped>
+@import '../../sass/partials/mixin';
+  .nav {
+    padding-left: 32px;
+    @include mobile {
+      padding-left: 12px;
+    }
+    .category {
+      i.i-menu {
+        padding-left: 5px;
+      }
+    }
+
+    }
+    .nav-container {
+      display: flex;
+      flex-flow: row wrap;
+      @include mobile {
+        flex-flow: column wrap;
+        padding-left: 24px;
+      }
+      .nav-item {
+        margin-right: 24px;
+        list-style: none;
+        @include mobile {
+          margin-bottom: 16px;
+          font-size: 20px;
+        }
+      }
+  }
 </style>
