10f9aa4fbc63ed254a4a1b9c8dbb0b65b13bd683
codemacaron <serom_kim100@naver.com>
2019-09-16 21:26:51 +0900

    페이지네이션 및 리스트 뷰 생성

---
+++ src/components/common/pagination.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+  .pagination
+    button.page-first(type="button") 맨 처음 페이지로 가기
+    button.page-prev(type="button") 이전 페이지로 가기
+    ul
+      li(v-for="i in 9")
+        a.page-num(href="#") {{ i }}
+      li
+        a.page-num.active(href="#") 10
+    button.page-next(type="button") 다음 페이지로 가기
+    button.page-last(type="button") 맨 끝 페이지로 가기
+
+</template>
+
+<script>
+export default {
+  name: "pagination",
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+@import "./../../sass/App";
+.pagination {
+  display: flex;
+  justify-content: center;
+  ul {
+    display: flex;
+    li {
+      a {
+        display: block;
+      }
+    }
+  }
+  [class^="page-"] {
+    display: inline-block;
+    width: 34px;
+    height: 34px;
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+    border-right: 1px solid #ddd;
+    border-left: 0 none;
+    background: #fff;
+  }
+  .page-first {
+    border-left: 1px solid #ddd;
+    background-image: url("https://res.kurly.com/pc/etc/old/images/common/icon-pagination-first.png");
+  }
+  .page-prev {
+    background-image: url("https://res.kurly.com/pc/etc/old/images/common/icon-pagination-prev.png");
+  }
+  .page-next {
+    background-image: url("https://res.kurly.com/pc/etc/old/images/common/icon-pagination-next.png");
+  }
+  .page-last {
+    background-image: url("https://res.kurly.com/pc/etc/old/images/common/icon-pagination-last.png");
+  }
+  .page-first,
+  .page-prev,
+  .page-next,
+  .page-last {
+    text-indent: -9999px;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    &:hover{
+      background-color: #f7f5f8;
+    }
+  }
+  .page-num {
+    text-align: center;
+    line-height: 34px;
+    text-decoration: unset;
+    color: #737373;
+    &.active,
+    &:hover {
+      background: #f7f5f8;
+      color: $color-point;
+    }
+  }
+}
+@include mobile {
+}
+
+@include tablet {
+}
+
+@include desktop {
+}
+</style>
---
+++ src/view/List.vue
@@ -0,0 +1,152 @@
+<template lang="pug">
+  #list
+    ul
+      li(v-for="i in 10")
+        a(href="#")
+          .thumb
+            img(src="https://images.unsplash.com/photo-1510130387422-82bed34b37e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80")
+            button.cart(type="cart") 장바구니
+            p.sale.sale10
+              em save
+              strong 20%
+          .info
+            h1 [기획상품] One-Frozen 노르웨이 고등어
+            p.cost 
+              span.dc 2,500원
+              span.emph →
+              span.price 2,000원
+            p.text 촉촉함이 남다른 한정판 고등어
+    c-pagination
+
+</template>
+
+<script>
+import CPagination from "./../components/common/pagination";
+export default {
+  name: "List",
+  methods: {},
+  components: { CPagination }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../sass/App";
+#list {
+  ul {
+    width: 960px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    li {
+      width: 300px;
+      height: 611px;
+      margin-top: 25px;
+      margin-right: 30px;
+      &:nth-of-type(3n) {
+        margin-right: 0;
+      }
+      a {
+        display: block;
+        width: 300px;
+        text-decoration: unset;
+        .thumb {
+          position: relative;
+          height: 396px;
+          border: 1px solid #cfcfcf;
+          background: #f9f8f9;
+          img {
+            width: 100%;
+          }
+          .cart {
+            position: absolute;
+            bottom: 10px;
+            right: 10px;
+            display: block;
+            overflow: hidden;
+            width: 45px;
+            height: 45px;
+            border: 0 none;
+            text-indent: -10000%;
+            font-size: 1px;
+            background: url("https://res.kurly.com/pc/etc/old/images/common/cart-add-button_type2.png")
+              no-repeat 50% 50%;
+          }
+          .sale {
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: flex;
+            flex-flow: column;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+            width: 70px;
+            height: 70px;
+            background: $color-point;
+            em,
+            strong {
+              display: block;
+              text-align: center;
+            }
+            em {
+              font-style: italic;
+              font-size: 23px;
+              font-weight: 100;
+              color: rgba(#fff, 0.8);
+            }
+            strong {
+              font-size: 23px;
+              color: #fff;
+            }
+          }
+        }
+        .info {
+          padding: 10px;
+          h1 {
+            font-weight: 700;
+            font-size: 20px;
+            color: #4c4c4c;
+            line-height: 29px;
+            margin: 0;
+          }
+          .cost {
+            padding-top: 9px;
+            font-size: 17px;
+            line-height: 24px;
+            font-weight: 700;
+            color: $color-point;
+            .dc {
+              font-weight: 700;
+              color: #999;
+              text-decoration: line-through;
+            }
+            .emph {
+              font-weight: 700;
+              color: $color-point;
+              padding: 0 5px;
+            }
+            .price {
+              font-weight: 700;
+              color: $color-point;
+            }
+          }
+          .text {
+            padding-top: 8px;
+            font-size: 13px;
+            color: #4c4c4c;
+            line-height: 19px;
+          }
+        }
+      }
+    }
+  }
+}
+@include mobile {
+}
+
+@include tablet {
+}
+
+@include desktop {
+}
+</style>
